98082c71d64610809d252b0e14757e3d3d08d515
Alison Diego <110138219+alisondiegodev@users.noreply.github.com>
2023-01-15 20:05:22 -0300

    logica
    working on

--- index.php
+++ index.php
@@ -13,140 +13,200 @@ date_default_timezone_set('America/Sao_Paulo');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ponto</title>
     <link rel="stylesheet" href="./style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@1,900&family=Poppins:wght@400;600&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@1,900&family=Poppins:wght@400;600&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <div class="container">
-        <div class="box">
-            <p class="data"><?= date('d/m/Y ')?></p>
-        <p id="horario"><?php echo date("H:i:s") ?> </p>
-       
+        <div id="box" class="box">
+            <p class="data"><?= date('d/m/Y ') ?></p>
+            <p id="horario"><?php echo date("H:i:s") ?> </p>
+
             <h1>Registrar Ponto</h1>
             <ul class="lista-pontos">
-               <div class="flex-li"><li><p>Entrada: </p> <span id="entrada"></span> </li><span onclick="abre_modal('entrada')"> <svg  class="pen"xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="m7 17.013 4.413-.015 9.632-9.54c.378-.378.586-.88.586-1.414s-.208-1.036-.586-1.414l-1.586-1.586c-.756-.756-2.075-.752-2.825-.003L7 12.583v4.43zM18.045 4.458l1.589 1.583-1.597 1.582-1.586-1.585 1.594-1.58zM9 13.417l6.03-5.973 1.586 1.586-6.029 5.971L9 15.006v-1.589z"></path><path d="M5 21h14c1.103 0 2-.897 2-2v-8.668l-2 2V19H8.158c-.026 0-.053.01-.079.01-.033 0-.066-.009-.1-.01H5V5h6.847l2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2z"></path></svg></span> </div> 
+                <div class="flex-li">
+                    <li>
+                        <p>Entrada: </p> <span id="entrada"></span>
+                    </li><span onclick="abre_modal('entrada')"> <svg class="pen" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);">
+                            <path d="m7 17.013 4.413-.015 9.632-9.54c.378-.378.586-.88.586-1.414s-.208-1.036-.586-1.414l-1.586-1.586c-.756-.756-2.075-.752-2.825-.003L7 12.583v4.43zM18.045 4.458l1.589 1.583-1.597 1.582-1.586-1.585 1.594-1.58zM9 13.417l6.03-5.973 1.586 1.586-6.029 5.971L9 15.006v-1.589z"></path>
+                            <path d="M5 21h14c1.103 0 2-.897 2-2v-8.668l-2 2V19H8.158c-.026 0-.053.01-.079.01-.033 0-.066-.009-.1-.01H5V5h6.847l2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2z"></path>
+                        </svg></span>
+                </div>
 
-               <div class="flex-li"> <li><p>Saida Intervalo: </p> <span id="saida_intervalo"></span></li> <svg  class="pen"xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="m7 17.013 4.413-.015 9.632-9.54c.378-.378.586-.88.586-1.414s-.208-1.036-.586-1.414l-1.586-1.586c-.756-.756-2.075-.752-2.825-.003L7 12.583v4.43zM18.045 4.458l1.589 1.583-1.597 1.582-1.586-1.585 1.594-1.58zM9 13.417l6.03-5.973 1.586 1.586-6.029 5.971L9 15.006v-1.589z"></path><path d="M5 21h14c1.103 0 2-.897 2-2v-8.668l-2 2V19H8.158c-.026 0-.053.01-.079.01-.033 0-.066-.009-.1-.01H5V5h6.847l2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2z"></path></svg> </div> 
+                <div class="flex-li">
+                    <li>
+                        <p>Saida Intervalo: </p> <span id="saida_intervalo"></span>
+                    </li><span onclick="abre_modal('saida_intervalo')"> <svg class="pen" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);">
+                            <path d="m7 17.013 4.413-.015 9.632-9.54c.378-.378.586-.88.586-1.414s-.208-1.036-.586-1.414l-1.586-1.586c-.756-.756-2.075-.752-2.825-.003L7 12.583v4.43zM18.045 4.458l1.589 1.583-1.597 1.582-1.586-1.585 1.594-1.58zM9 13.417l6.03-5.973 1.586 1.586-6.029 5.971L9 15.006v-1.589z"></path>
+                            <path d="M5 21h14c1.103 0 2-.897 2-2v-8.668l-2 2V19H8.158c-.026 0-.053.01-.079.01-.033 0-.066-.009-.1-.01H5V5h6.847l2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2z"></path>
+                        </svg></span>
+                </div>
 
-               <div class="flex-li"> <li><p>Retorno Intervalo: </p> <span id="retorno_intervalo"></span></li><svg  class="pen"xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="m7 17.013 4.413-.015 9.632-9.54c.378-.378.586-.88.586-1.414s-.208-1.036-.586-1.414l-1.586-1.586c-.756-.756-2.075-.752-2.825-.003L7 12.583v4.43zM18.045 4.458l1.589 1.583-1.597 1.582-1.586-1.585 1.594-1.58zM9 13.417l6.03-5.973 1.586 1.586-6.029 5.971L9 15.006v-1.589z"></path><path d="M5 21h14c1.103 0 2-.897 2-2v-8.668l-2 2V19H8.158c-.026 0-.053.01-.079.01-.033 0-.066-.009-.1-.01H5V5h6.847l2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2z"></path></svg> </div> 
+                <div class="flex-li">
+                    <li>
+                        <p>Retorno Intervalo: </p> <span id="retorno_intervalo"></span>
+                    </li><span onclick="abre_modal('retorno_intervalo')"> <svg class="pen" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);">
+                            <path d="m7 17.013 4.413-.015 9.632-9.54c.378-.378.586-.88.586-1.414s-.208-1.036-.586-1.414l-1.586-1.586c-.756-.756-2.075-.752-2.825-.003L7 12.583v4.43zM18.045 4.458l1.589 1.583-1.597 1.582-1.586-1.585 1.594-1.58zM9 13.417l6.03-5.973 1.586 1.586-6.029 5.971L9 15.006v-1.589z"></path>
+                            <path d="M5 21h14c1.103 0 2-.897 2-2v-8.668l-2 2V19H8.158c-.026 0-.053.01-.079.01-.033 0-.066-.009-.1-.01H5V5h6.847l2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2z"></path>
+                        </svg> </span>
+                </div>
 
-               <div class="flex-li"> <li><p>Saida: </p> <span id="saida"></span></li><svg  class="pen"xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="m7 17.013 4.413-.015 9.632-9.54c.378-.378.586-.88.586-1.414s-.208-1.036-.586-1.414l-1.586-1.586c-.756-.756-2.075-.752-2.825-.003L7 12.583v4.43zM18.045 4.458l1.589 1.583-1.597 1.582-1.586-1.585 1.594-1.58zM9 13.417l6.03-5.973 1.586 1.586-6.029 5.971L9 15.006v-1.589z"></path><path d="M5 21h14c1.103 0 2-.897 2-2v-8.668l-2 2V19H8.158c-.026 0-.053.01-.079.01-.033 0-.066-.009-.1-.01H5V5h6.847l2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2z"></path></svg> </div> 
+                <div class="flex-li">
+                    <li>
+                        <p>Saida: </p> <span id="saida"></span>
+                    </li><span onclick="abre_modal('saida')"> <svg class="pen" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);">
+                            <path d="m7 17.013 4.413-.015 9.632-9.54c.378-.378.586-.88.586-1.414s-.208-1.036-.586-1.414l-1.586-1.586c-.756-.756-2.075-.752-2.825-.003L7 12.583v4.43zM18.045 4.458l1.589 1.583-1.597 1.582-1.586-1.585 1.594-1.58zM9 13.417l6.03-5.973 1.586 1.586-6.029 5.971L9 15.006v-1.589z"></path>
+                            <path d="M5 21h14c1.103 0 2-.897 2-2v-8.668l-2 2V19H8.158c-.026 0-.053.01-.079.01-.033 0-.066-.009-.1-.01H5V5h6.847l2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2z"></path>
+                        </svg></span>
+                </div>
 
-                <li><p>Horario Limite: </p> <span id="limite"></span></li>
+                <li>
+                    <p>Horario Limite: </p> <span id="limite"></span>
+                </li>
 
             </ul>
             <button class="registrar-btn" onclick="registra_horario()">Registrar Ponto</button>
 
+
+
+        </div>
         <div id="input-box" class="edit-box">
             <h3 class="editar-titulo">Editar Horario</h3>
-        <input type="time" id="input" name="horario">
-        <button id="salvar">Salvar</button>
+            <input type="time" id="input" name="horario">
+            <button id="salvar">Salvar</button>
 
 
         </div>
+    </div>
 
-          </div>
-
-        </div>
-
-        <script
-  src="https://code.jquery.com/jquery-3.6.3.min.js"
-  integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU="
-  crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
     <script>
-  var limite = document.getElementById('limite');
+        var limite = document.getElementById('limite');
         var entrada = $('#entrada').html();
         var saida_intervalo = $('#saida_intervalo').html();
         var retorno_intervalo = $('#retorno_intervalo').html();
         var saida = $('#saida').html();
 
-      // EDITA HORARIO
-      function abre_modal(id) {
-        var modal = document.getElementById('input-box');
-        var itemAlterar = document.getElementById(id);;
-        var salvar = document.getElementById('salvar');
-        modal.style.display = "flex";
+        // EDITA HORARIO
+        function abre_modal(id) {
 
-        salvar.addEventListener('click', edita_horario);
+            var modal = document.getElementById('input-box');
+            var itemAlterar = document.getElementById(id);;
+            var salvar = document.getElementById('salvar');
+            var box = document.getElementById('box');
+            modal.style.display = "flex";
+            box.style.backgroundColor = "unset"
 
-        function edita_horario() {
-        var inputUsuario = document.getElementById('input').value;
-        
-        console.log(inputUsuario)
+            salvar.addEventListener('click', edita_horario);
 
-        if (id = "entrada") {
-console.log("entrou na entrada")
-        }else {
-            console.log("não entrou")
-
-        
-        itemAlterar.innerHTML = inputUsuario;
-    }
-    }
+            function edita_horario() {
+                var modal = document.getElementById('input-box');
+                var box = document.getElementById('box');
+                box.style.backgroundColor = "#fff"
 
 
+                var inputUsuario = document.getElementById('input').value;
+                if (inputUsuario == "") {
+                    console.log("vazio")
+                    modal.style.display = "none";
+
+                } else {
+
+                    var time = inputUsuario.split(":");
+                    inputToDate = new Date();
+                    inputToDate.setHours(time[0]);
+                    inputToDate.setMinutes(time[1]);
+
+                    if (id == "entrada") {
+                        itemAlterar.innerHTML = inputToDate.toLocaleTimeString('pt-br', {
+                            timezone: "America/Sao_Paulo",
+                            hour12: true
+                        })
+                        inputToDate.setHours(inputToDate.getHours() + 10);
+                        inputToDate.setMinutes(inputToDate.getMinutes() + 10);
+                        limite.innerHTML = inputToDate.toLocaleTimeString('pt-br', {
+                            timezone: "America/Sao_Paulo",
+                            hour12: true
 
 
+                        })
+                        modal.style.display = "none";
+                        box.style.backgroundColor = "#fff"
 
 
-      }
+                    } else {
 
-      
-
-//PEGA ELEMENOS DO HTML
-      
-//REGISTRA HORARIO
-      function registra_horario() {
-        var entrada = $('#entrada').html();
-        var saida_intervalo = $('#saida_intervalo').html();
-        var retorno_intervalo = $('#retorno_intervalo').html();
-        var saida = $('#saida').html();
-
-        if (entrada == "") {
-            console.log('entrou no primeiro if')
-            var data = new Date().toLocaleTimeString("pt-br", {
-                timeZone: "America/Sao_Paulo",
-                hour12: true
-            });
-            var formatarData = data.replace(", ", " - ");
-            document.getElementById('entrada').innerHTML = formatarData;
-            horarioLimite(formatarData);
+                        console.log("não entrou")
 
 
-        } else if (saida_intervalo == "" ){
-            console.log("entrou no segundo if");
-            var data = new Date().toLocaleTimeString("pt-br", {
-                timeZone: "America/Sao_Paulo",
-                hour12: true
-            });
-            var formatarData = data.replace(", ", " - ");
-            $("#saida_intervalo").html(formatarData);
-            horarioLimite(formatarData);
+                        itemAlterar.innerHTML = inputToDate.toLocaleTimeString('pt-br', {
+                            timezone: "America/Sao_Paulo",
+                            hour12: true
 
 
-        } else if (retorno_intervalo ==""){ 
-            console.log("entrou no terceiro if")
-            var data = new Date().toLocaleTimeString("pt-br", {
-                timeZone: "America/Sao_Paulo",
-                hour12: true
-            });
-            var formatarData = data.replace(", ", " - ");
-            $("#retorno_intervalo").html(formatarData);
-            horarioLimite(formatarData);
-        } else if (saida ==""){ 
-            console.log("entrou na saida")
-            var data = new Date().toLocaleTimeString("pt-br", {
-                timeZone: "America/Sao_Paulo",
-                hour12: true
-            });
-            var formatarData = data.replace(", ", " - ");
+                        })
+                        modal.style.display = "none";
+                    }
+                }
 
-            $("#saida").html(formatarData);
-            horarioLimite(formatarData);
-        } else {
-            console.log("consegui :)")
+            }
         }
-      }
+
+
+
+        //PEGA ELEMENOS DO HTML
+
+        //REGISTRA HORARIO
+        function registra_horario() {
+            var entrada = $('#entrada').html();
+            var saida_intervalo = $('#saida_intervalo').html();
+            var retorno_intervalo = $('#retorno_intervalo').html();
+            var saida = $('#saida').html();
+
+            if (entrada == "") {
+                console.log('entrou no primeiro if')
+                var data = new Date().toLocaleTimeString("pt-br", {
+                    timeZone: "America/Sao_Paulo",
+                    hour12: true
+                });
+                var formatarData = data.replace(", ", " - ");
+                document.getElementById('entrada').innerHTML = formatarData;
+                horarioLimite(formatarData);
+
+
+            } else if (saida_intervalo == "") {
+                console.log("entrou no segundo if");
+                var data = new Date().toLocaleTimeString("pt-br", {
+                    timeZone: "America/Sao_Paulo",
+                    hour12: true
+                });
+                var formatarData = data.replace(", ", " - ");
+                $("#saida_intervalo").html(formatarData);
+
+
+            } else if (retorno_intervalo == "") {
+                console.log("entrou no terceiro if")
+                var data = new Date().toLocaleTimeString("pt-br", {
+                    timeZone: "America/Sao_Paulo",
+                    hour12: true
+                });
+                var formatarData = data.replace(", ", " - ");
+                $("#retorno_intervalo").html(formatarData);
+            } else if (saida == "") {
+                console.log("entrou na saida")
+                var data = new Date().toLocaleTimeString("pt-br", {
+                    timeZone: "America/Sao_Paulo",
+                    hour12: true
+                });
+                var formatarData = data.replace(", ", " - ");
+
+                $("#saida").html(formatarData);
+            } else {
+                console.log("consegui :)")
+            }
+        }
 
 
         //PEGA ELEMENTO HORARIO
@@ -166,20 +226,16 @@ console.log("entrou na entrada")
         function horarioLimite(formatarData) {
             var date = new Date();
             var hora_limite = new Date(date.setHours(date.getHours() + 9));
-            limite.innerHTML = hora_limite.toLocaleTimeString("pt-br", {   
-            timeZone: "America/Sao_Paulo",
+            limite.innerHTML = hora_limite.toLocaleTimeString("pt-br", {
+                timeZone: "America/Sao_Paulo",
                 hour12: true
-        });
-        console.log(hora_limite.toLocaleTimeString("pt-br", {   
-            timeZone: "America/Sao_Paulo",
+            });
+            console.log(hora_limite.toLocaleTimeString("pt-br", {
+                timeZone: "America/Sao_Paulo",
                 hour12: true
-        }) );
-  
+            }));
+
         }
-
-
-
-
     </script>
 </body>
 
